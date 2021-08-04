be4511a8666d86003dd1c8448dc688b7e136a2ff
Siddhanth Bhimakari <68469566+SiddhanthNB@users.noreply.github.com>
2021-08-04 22:13:34 +0530

    Final Commit

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,22 +1,19 @@
-from flask import Flask, redirect, url_for, render_template, request
-
-"""from skimage import io
+from skimage import io
 from picamera import PiCamera
 from time import sleep
 import RPi.GPIO as GPIO
-# from turnon import relay_on
-# from turnof import relay_off
+from flask import Flask, redirect, url_for, render_template, request, jsonify
+import json
+
 from image_processing import ImageProc
 
 GPIO.setwarnings(False)
-
-camera = PiCamera()"""
+#camera = PiCamera()
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    #return redirect(url_for("login"))
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'uvc' or request.form['password'] != 'kleitece':
@@ -29,55 +26,77 @@ def home():
 def input():
     if request.method == "POST":
         time = int(request.form["nm"])
-        res = { "label1": time,
-        "label2": 2,
-        "label3": 3
-                }
-        
-        """GPIO.setmode(GPIO.BCM)
+        """
+        #relay setup
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(21, GPIO.OUT)
+        
+        #setting uvc light on
         GPIO.output(21, GPIO.LOW)
         sleep(3)
+        
+        #trigerring camera
         camera.start_preview()
         sleep(5)
-        camera.capture("/home/pi/project/images/test1.jpg")
+        camera.capture("/home/pi/project/flask_app/images/before.jpg")
         camera.stop_preview()
-
-
-        sleep(time)
-
+        
+        #uvc treatment
+        sleep(1200)
+        
+        #trigerring camera
         camera.start_preview()
         sleep(5)
-        camera.capture("/home/pi/project/images/test2.jpg")
+        camera.capture("/home/pi/project/flask_app/images/after.jpg")
         camera.stop_preview()
+        
+        #setting uvc light off
         sleep(3)
         GPIO.output(21, GPIO.HIGH)
         GPIO.cleanup()
+        """
+        return redirect(url_for("output"))
+    return render_template("interface.html")
 
 
-        before = io.imread("/home/pi/project/images/test1.jpg")
-        after = io.imread("/home/pi/project/images/test2.jpg")
-
-        before, after = ImageProc.pre_processing(before, after)
-        mse, ssi =  ImageProc.compare(before, after)
-        if ssi == 1.00 or mse == 0.00:
-            res = { "status" : "Failure",
-                "MSE Value" :  mse,
-                "SSI Value" : ssi
-                }
-        else:
-            res = { "status" : "Success",
-                    "MSE Value" :  mse,
-                    "SSI Value" : ssi
-            }"""
-        return redirect(url_for("days", result=res ))
+@app.route("/output", methods=['GET', 'POST'])
+def output():
+    #reading both images 
+    #before = io.imread("/home/pi/project/flask_app/images/before.jpg")
+    #after = io.imread("/home/pi/project/flask_app/images/after.jpg")
+    before = io.imread("https://raw.githubusercontent.com/SiddhanthNB/uvc-img-proc/main/images/IMG%201.jpg")
+    after = io.imread("https://raw.githubusercontent.com/SiddhanthNB/uvc-img-proc/main/images/IMG%202.jpg")
+    io.imsave("/home/pi/project/flask_app/images/before.jpg", after)
+    
+    before, after = ImageProc.pre_processing(before, after)
+    io.imsave("/home/pi/project/flask_app/images/edge_before.jpg", before)
+    io.imsave("/home/pi/project/flask_app/images/edge_after.jpg", after)
+    
+    mse, ssi =  ImageProc.compare(before, after)
+    h_diff = ImageProc.hist_compare(before, after)
+    n_m, n_0 = ImageProc.compare_images(before, after)
+    
+    if ssi == 1.00:
+        result = { "status" : "Failure",
+            "SSI Value" : ssi,
+            "MSE Value" :  mse,
+            "Histogram diff": h_diff,
+            "Zero norm/pixel": n_0*1.0/after.size,
+            "Manhattan norm/pixel": n_m/before.size
+            }
+         
     else:
-        return render_template("interface.html")
-
-
-@app.route("/<result>")
-def days(result):
-    return f"<h2>{result}</h2>"
+        result = { "status" : "Success",
+            "SSI Value" : ssi,
+            "MSE Value" :  mse,
+            "Histogram diff": h_diff,
+            "Zero norm/pixel": n_0*1.0/after.size,
+            "Manhattan norm/pixel": n_m/before.size
+            }
+        #res = json.dumps(res)
+    
+    #return f"<h2>{result}</h2>"
+    return render_template("dict.html", result=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
